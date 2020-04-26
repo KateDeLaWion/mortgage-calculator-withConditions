c6396bb335495257a9ebb57055e2ca0601eba04e
musicstudio <musicstudio@musics-MBP.home>
2020-04-26 12:59:57 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,46 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
+        int principal;
+        float monthlyInterest;
+        int numberOfPayments;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while(true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal>=1000 && principal<=1_000_000) {
+                break;
+            }
+            else {
+                System.out.println("Enter an integer between 1,000 to 1,000,000");
+            }
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if(annualInterest>=1 && annualInterest<= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            else{
+                    System.out.println("Enter Annual Interest Rate between 1 and 30");
+                }
+        }
 
-        System.out.print("Period (Years of payment back): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+        while(true) {
+            System.out.print("Period (Years of payment back): ");
+            byte years = scanner.nextByte();
+            if(years>=1 && years<=30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            else {
+                System.out.println("Enter years between 1 to 30");
+            }
+        }
 
         double mortgage = principal * ( monthlyInterest * Math.pow(1+monthlyInterest,numberOfPayments))
                           / ( Math.pow(1+monthlyInterest,numberOfPayments) -1);
